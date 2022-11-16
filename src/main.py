f73911769e6dcb4fdd8eb5a901a4ede1ee179996
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-16 11:58:30 -0300

    REFACTOR: change ab2 to main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,13 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from goalpost import GoalPost
-import pygame
-from pygame.locals import *
+
+from drawGroundLines import *
+from dayAndNightLight import *
+from drawSoccerBall import *
+from drawTextStatus import *
+from drawGround import *
+from drawSky import *
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
@@ -12,22 +17,26 @@ blueTeamCounter = 0
 redTeamCounter = 0
 
 fovY = 75.0
-# cameraX, cameraY, cameraZ = 2.0, 3.7, -10.0
-cameraX, cameraY, cameraZ = 2.0, 3.7, 7.0
-wallX, wallY, wallZ = 0.0, 1.5, 0.0
+cameraX, cameraY, cameraZ = 2.0, 3.7, 12.4
+wallX, wallY, wallZ = 0.0, 2.0, 0.0
 maxCameraX, maxCameraY, maxCameraZ = 48.0, 10000, 12.0
 minCameraX, minCameraY, minCameraZ = -48.0, 0.0, 1.0
 
 wallRotationY = 95.0
 wallRotationZ = -90.0
 wallRotationX = 0
-wallWidth, wallHeight, wallDepth = 0.3, 15.0, 8.0
+wallWidth, wallHeight, wallDepth = 4.5, 2.0, 10.5
 
 centerX, centerY, centerZ = wallX, wallY, wallZ
-initialSoccerBallX, initialSoccerBallY, initialSoccerBallZ = wallX, wallY + 0.5, wallZ
+initialSoccerBallX, initialSoccerBallY, initialSoccerBallZ = 1.6, 2.1, 5.20
 soccerBallRotationX, soccerBallRotationY, soccerBallRotationZ = 0,0,0
 soccerBallX, soccerBallY, soccerBallZ = initialSoccerBallX, initialSoccerBallY, initialSoccerBallZ
 updateSoccerBallX, updateSoccerBallY, updateSoccerBallZ = 1,1,1
+
+isDay = True
+
+def init():
+  glClearColor(0.0, 0.0, 0.0, 1.0)
 
 def resetSoccerBallPosition():
   global soccerBallX 
@@ -35,102 +44,23 @@ def resetSoccerBallPosition():
   global soccerBallZ 
   soccerBallX, soccerBallY, soccerBallZ = initialSoccerBallX, initialSoccerBallY, initialSoccerBallZ
 
-def init():
-  glClearColor(0.0, 0.0, 0.0, 1.0)
-
-def drawTextStatus():
-  blueTeamText = f'TIME AZUL: {blueTeamCounter}'
-
-  redTeamText = f'TIME VERMELHO: {redTeamCounter}'
-
-  pygame.font.init()
-  font = pygame.font.SysFont('arial', 32)
-
-  x, y = 0, 32
-  textSurface = font.render(blueTeamText, True, (255, 255, 66, 255), (0, 66, 0, 255))
-  textData = pygame.image.tostring(textSurface, "RGBA", True)
-  glWindowPos2d(x, y)
-  glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
-
-  x, y = 0, 0 
-  textSurface = font.render(redTeamText, True, (255, 255, 66, 255), (0, 66, 0, 255))
-  textData = pygame.image.tostring(textSurface, "RGBA", True)
-  glWindowPos2d(x, y)
-  glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
-
-
-def drawGround():
-  # glPushMatrix()
-  glTranslatef(wallX, wallY, wallZ)
-  glRotatef(wallRotationY, 0.0, 1.0, 0.0)
-  glRotatef(wallRotationZ, 0.0, 0.0, 1.0)
-  glRotatef(wallRotationX, 1.0, 0.0, 0.0)
-  glScalef(wallWidth, wallHeight, wallDepth)
-
-  glEnable(GL_COLOR_MATERIAL)
-  glColor3f(0.0, 0.5, 0.2)
-  glutSolidCube(1.0)
-
-  mat_specular = [0.0, 0.0, 0.0]
-  mat_shininess = [0.0]
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
-
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-  # glPopMatrix()
-
-def drawSoccerBall():
-  glPushMatrix()
-  SLICES, STACKS = 15, 15
-  glEnable(GL_COLOR_MATERIAL)
-  ballColor = [1, 1, 1]
-  glColor3f(ballColor[0], ballColor[1], ballColor[2])
-  glTranslatef(soccerBallX, soccerBallY, soccerBallZ)
-  glRotatef(soccerBallRotationY, 0.0, 1.0, 0.0)
-  glRotatef(soccerBallRotationZ, 0.0, 0.0, 1.0)
-  glRotatef(soccerBallRotationX, 1.0, 0.0, 0.0)
-  glutWireSphere(0.1, SLICES, STACKS)
-  glutSolidSphere(0.1, SLICES, STACKS)
-  glColor3f(0, 0, 0)
-  glutSolidCube(0.125)
-  mat_specular = [0.0, 0.0, 0.0]
-  mat_shininess = [0.0]
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-  glPopMatrix()
-
-
-def drawSky():
-  # glPushMatrix()
-  SLICES, STACKS = 15, 15
-  glEnable(GL_COLOR_MATERIAL)
-  skyBlue = [0.53, 0.81, 0.92]
-  glColor3f(skyBlue[0], skyBlue[1], skyBlue[2])
-  glTranslatef(0, 0, 0 )
-  glutSolidSphere(50, SLICES, STACKS)
-  mat_specular = [0.0, 0.0, 0.0]
-  mat_shininess = [0.0]
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-  # glPopMatrix()
-
-
 def display():
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
   glEnable(GL_DEPTH_TEST)
   glMatrixMode(GL_MODELVIEW) 
   glLoadIdentity()
+  configureLight()
 
   gluLookAt(cameraX, cameraY, cameraZ, centerX, centerY, centerZ, 0.0, 1.0, 0.0)
-  drawTextStatus()
-  drawSoccerBall()
+  drawTextStatus(blueTeamCounter, redTeamCounter)
+  drawSoccerBall(soccerBallX, soccerBallY, soccerBallZ, soccerBallRotationX, soccerBallRotationY, soccerBallRotationZ)
   gp1 = GoalPost('blue')
   gp1.draw()
   gp2 = GoalPost('red')
   gp2.draw()
-  drawGround() 
+  drawGroundLines()
+  drawGrandStand()
+  drawGround(wallWidth, wallHeight, wallDepth)
   drawSky()
 
   glutSwapBuffers()
@@ -142,6 +72,22 @@ def idleDisplay():
 def reshape(w, h):
   pass
 
+def gameStatus():
+  print("PLACAR: ")
+  print("AZUL: ", blueTeamCounter)
+  print("VERMELHO: ", redTeamCounter)
+
+def configureLight():
+  global cameraX 
+  global cameraY 
+  global cameraZ 
+  global isDay
+  if (isDay):
+    dayLight(cameraX, cameraY, cameraZ)
+  else:
+    nightLight(cameraX, cameraY, cameraZ)
+
+
 def keyCallback(key, x, y):
   global cameraZ 
   global soccerBallX 
@@ -152,33 +98,32 @@ def keyCallback(key, x, y):
   global soccerBallRotationZ 
   global blueTeamCounter 
   global redTeamCounter 
-  offset = 0.2
+  global isDay
+  offset = 0.1
   rotationOffset = 20
   if (key == b'a'):
     soccerBallX -= offset
     soccerBallRotationZ -= rotationOffset
-    # print("Hello Key A", soccerBallX)
   if (key == b'd'):
     soccerBallX += offset
     soccerBallRotationZ += rotationOffset
-    # print("Hello Key D", soccerBallX)
   if (key == b'w'):
     soccerBallZ -= offset
     soccerBallRotationX -= rotationOffset
-    # print("Hello Key W", soccerBallZ)
   if (key == b's'):
     soccerBallZ += offset
     soccerBallRotationX += rotationOffset
-    # print("Hello Key S", soccerBallZ)
   if (key == b'e'):
-    # print("Hello Key E")
     cameraZ -= offset
   if (key == b'q'):
     cameraZ += offset
-    # print("Hello Key Q")
+  if (key == b'n'):
+    isDay = not isDay
+  print(soccerBallX, soccerBallY, soccerBallZ)
+  print(cameraX, cameraY, cameraZ)
 
-  blueTeamGoal = -6.4
-  redTeamGoal = 6.4
+  blueTeamGoal = 0.20000000000000193
+  redTeamGoal = 10.2
 
   if (soccerBallZ <= blueTeamGoal):
     blueTeamCounter += 1
@@ -191,20 +136,22 @@ def keyCallback(key, x, y):
     
   glutPostRedisplay()
 
-def configureLight():
-	light_ambient = [0.2, 0.2, 0.2]
-	light_position = [cameraX, cameraY, cameraZ, 0.0]
-	
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient)
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position)
-	
-	glEnable(GL_LIGHTING)
-	glEnable(GL_LIGHT0)
+def drawGrandStand():
+  glPushMatrix()
+  glTranslatef(-5.0, 2.0, 2.0)
+  glScalef(0.5, 4.0, 16.0)
 
-def gameStatus():
-  print("PLACAR: ")
-  print("AZUL: ", blueTeamCounter)
-  print("VERMELHO: ", redTeamCounter)
+  glEnable(GL_COLOR_MATERIAL)
+  glColor3f(0.4, 0.4, 0.4)
+  glutSolidCube(1.0)
+
+  mat_specular = [0.0, 0.0, 0.0]
+  mat_shininess = [0.0]
+  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+
+  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
+  glPopMatrix()
 
 
 def main():
@@ -222,8 +169,7 @@ def main():
   glLoadIdentity()
   gluPerspective(fovY, 1.0 * WINDOW_WIDTH / WINDOW_HEIGHT, 0.001, 1000.0)
   glutKeyboardFunc(keyCallback)
-  configureLight()
-  gameStatus()
+
   glutMainLoop()
   
 

739ac3793c0bb1be44885613f5380bbdea2148b7
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-18 10:03:17 -0300

    REFACTOR: separate texture on drawGround

--- src/drawGround.py
+++ src/drawGround.py
@@ -3,40 +3,17 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import numpy
 from PIL import Image
+from readTexture import *
 
-def drawGround(wallWidth, wallHeight, wallDepth):
-  glPushMatrix()
-  # texture = glGenTextures(1)
-  # glBindTexture(GL_TEXTURE_2D, texture)
-  # # set the texture wrapping/filtering options (on the currently bound texture object)
-  # glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)	
-  # glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-  # glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-  # glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-  # img = Image.open('./src/tex-blue.jpg')
-  # img_data = numpy.array(list(img.getdata()), numpy.int8)
-  # textID = glGenTextures(1)
-  # glBindTexture(GL_TEXTURE_2D,textID)
-  # glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-  # glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 200, 150, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
-  # glGenerateMipmap(GL_TEXTURE_2D)
-  glTranslatef(wallWidth/3, wallHeight/2 , (wallDepth/2) )
-  glScalef(wallWidth, wallHeight, wallDepth)
+def drawGround():
+  texture_0 = read_texture('./src/grass.jpg')
+  glEnable(GL_TEXTURE_2D)
+  glBegin(GL_QUADS)
 
-  glEnable(GL_COLOR_MATERIAL)
-  glColor3f(0.0, 0.5, 0.2)
-  # glEnable(GL_TEXTURE_GEN_S) #enable texture coordinate generation
-  # glEnable(GL_TEXTURE_GEN_T)
-  # glBindTexture(GL_TEXTURE_2D, textID)
-  # glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0)
-  glutSolidCube(1)
-  # glDisable(GL_TEXTURE_GEN_S) #enable texture coordinate generation
-  # glDisable(GL_TEXTURE_GEN_T)
+  glTexCoord2f(0.0, 20.0); glVertex3f(-0.75, 2.0, 0.05)
+  glTexCoord2f(20.0, 0.0); glVertex3f(-0.75, 2.0, 16.0)
+  glTexCoord2f(0.0, 0.0); glVertex3f(3.75, 2.0, 16.0)
+  glTexCoord2f(20.0, 20.0); glVertex3f(3.75, 2.0, 0.05)
 
-  mat_specular = [0.0, 0.0, 0.0]
-  mat_shininess = [0.0]
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
-
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-  glPopMatrix()
+  glEnd()
+  glDisable(GL_TEXTURE_2D)
--- src/main.py
+++ src/main.py
@@ -56,17 +56,7 @@ def display():
   configureLight()
 
   gluLookAt(cameraX, cameraY, cameraZ, centerX, centerY, centerZ, 0.0, 1.0, 0.0)
-  texture_0 = read_texture('./src/grass.jpg')
-  glEnable(GL_TEXTURE_2D)
-  glBegin(GL_QUADS)
 
-  glTexCoord2f(0.0, 20.0); glVertex3f(-0.75, 2.0, 0.0)
-  glTexCoord2f(20.0, 0.0); glVertex3f(-0.75, 2.0, 16.0)
-  glTexCoord2f(0.0, 0.0); glVertex3f(3.75, 2.0, 16.0)
-  glTexCoord2f(20.0, 20.0); glVertex3f(3.75, 2.0, 0.0)
-
-  glEnd()
-  glDisable(GL_TEXTURE_2D)
   drawTextStatus(blueTeamCounter, redTeamCounter)
   drawSoccerBall(soccerBallX, soccerBallY, soccerBallZ, soccerBallRotationX, soccerBallRotationY, soccerBallRotationZ)
   gp1 = GoalPost('blue')
@@ -77,7 +67,7 @@ def display():
   drawGrandStandLeft()
   drawGrandStandRight()
   drawGrandStandTop()
-  # drawGround(wallWidth, wallHeight, wallDepth)
+  drawGround()
   drawSky(isDay)
   drawMoon(isDay)
 

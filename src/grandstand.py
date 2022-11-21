64aa876e48709234dd48dcfc6d8e91d231e048e4
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-21 09:32:16 -0300

    FEAT: change grandstand texture

--- src/grandstand.py
+++ src/grandstand.py
@@ -3,35 +3,35 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from readTexture import *
 
-total = 10
+total = 7
 color = [0.0, 0.7, 0.7]
 
 def drawGrandStandLeft():
-  bricks = read_texture('./src/ground-gray.jpg')
+  bricks = read_texture('./src/texture-red.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
     xOffset = 0.5 * i
     yOffset = 0.25 * i
 
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5.0 + xOffset, 5.0 - yOffset, 16.0)
-    glTexCoord2f(100.0, 0.0); glVertex3f(-5.0 + xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(100.0, 100.0); glVertex3f(-5.0 + xOffset, 3.0 - yOffset, -4.4)
-    glTexCoord2f(0.0, 100.0); glVertex3f(-5.0 + xOffset, 3.0 - yOffset, 16.0)
+    glTexCoord2f(0.0, 0.0); glVertex3f(-3.5 + xOffset, 4.0 - yOffset, 16.0)
+    glTexCoord2f(100.0, 0.0); glVertex3f(-3.5 + xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(100.0, 100.0); glVertex3f(-3.5 + xOffset, 0.0 - yOffset, -4.4)
+    glTexCoord2f(0.0, 100.0); glVertex3f(-3.5 + xOffset, 0.0 - yOffset, 16.0)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)
 
-  bricks = read_texture('./src/texture-green.jpg')
+  bricks = read_texture('./src/texture-white.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
     xOffset = 0.5 * i
     yOffset = 0.25 * i
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5.0 + xOffset, 5.0 - yOffset, 16.0)
-    glTexCoord2f(100.0, 0.0); glVertex3f(-5.0 + xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(100.0, 100.0); glVertex3f(-5.5 + xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(0.0, 100.0); glVertex3f(-5.5 + xOffset, 5.0 - yOffset, 16.0)
+    glTexCoord2f(0.0, 0.0); glVertex3f(-3.5 + xOffset, 4.0 - yOffset, 16.0)
+    glTexCoord2f(100.0, 0.0); glVertex3f(-3.5 + xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(100.0, 100.0); glVertex3f(-4.0 + xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(0.0, 100.0); glVertex3f(-4.0 + xOffset, 4.0 - yOffset, 16.0)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)
@@ -39,37 +39,37 @@ def drawGrandStandLeft():
 
 
 def drawGrandStandRight():
-  bricks = read_texture('./src/ground-gray.jpg')
+  bricks = read_texture('./src/texture-red.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
     xOffset = 0.5 * i
     yOffset = 0.25 * i
 
-    glTexCoord2f(0.0, 0.0); glVertex3f(8.5 - xOffset, 5.0 - yOffset, 16.0)
-    glTexCoord2f(100.0, 0.0); glVertex3f(8.5 - xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(100.0, 100.0); glVertex3f(8.5 - xOffset, 3.0 - yOffset, -4.4)
-    glTexCoord2f(0.0, 100.0); glVertex3f(8.5 - xOffset, 3.0 - yOffset, 16.0)
+    glTexCoord2f(0.0, 0.0); glVertex3f(7.0 - xOffset, 4.0 - yOffset, 16.0)
+    glTexCoord2f(100.0, 0.0); glVertex3f(7.0 - xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(100.0, 100.0); glVertex3f(7.0 - xOffset, 0.0 - yOffset, -4.4)
+    glTexCoord2f(0.0, 100.0); glVertex3f(7.0 - xOffset, 0.0 - yOffset, 16.0)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)
 
-  bricks = read_texture('./src/texture-green.jpg')
+  bricks = read_texture('./src/texture-white.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
     xOffset = 0.5 * i
     yOffset = 0.25 * i
-    glTexCoord2f(0.0, 0.0); glVertex3f(9.0 - xOffset, 5.0 - yOffset, 16.0)
-    glTexCoord2f(100.0, 0.0); glVertex3f(9.0 - xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(100.0, 100.0); glVertex3f(8.5 - xOffset, 5.0 - yOffset, -4.4)
-    glTexCoord2f(0.0, 100.0); glVertex3f(8.5 - xOffset, 5.0 - yOffset, 16.0)
+    glTexCoord2f(0.0, 0.0); glVertex3f(7.5 - xOffset, 4.0 - yOffset, 16.0)
+    glTexCoord2f(100.0, 0.0); glVertex3f(7.5 - xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(100.0, 100.0); glVertex3f(7.0 - xOffset, 4.0 - yOffset, -4.4)
+    glTexCoord2f(0.0, 100.0); glVertex3f(7.0 - xOffset, 4.0 - yOffset, 16.0)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)
 
 def drawGrandStandTop():
-  bricks = read_texture('./src/ground-gray.jpg')
+  bricks = read_texture('./src/texture-red.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
@@ -77,15 +77,15 @@ def drawGrandStandTop():
     zOffset = 0.5 * i
     yOffset = 0.25 * i
 
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5.1, 5.0 -yOffset, -4.45 + zOffset)
-    glTexCoord2f(100.0, 0.0); glVertex3f(-5.1, 3.0 -yOffset, -4.45+ zOffset)
-    glTexCoord2f(100.0, 100.0); glVertex3f(10.0, 3.0 -yOffset, -4.45+ zOffset)
-    glTexCoord2f(0.0, 100.0); glVertex3f(10.0, 5.0 -yOffset, -4.45 + zOffset)
+    glTexCoord2f(0.0, 0.0); glVertex3f(10.0, 4.0 -yOffset, -3.0 + zOffset)
+    glTexCoord2f(100.0, 100.0); glVertex3f(10.0, 0.0 -yOffset, -3.0+ zOffset)
+    glTexCoord2f(100.0, 0.0); glVertex3f(-5.1, 0.0 -yOffset, -3.0+ zOffset)
+    glTexCoord2f(0.0, 100.0); glVertex3f(-5.1, 4.0 -yOffset, -3.0 + zOffset)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)
 
-  bricks = read_texture('./src/texture-green.jpg')
+  bricks = read_texture('./src/texture-white.jpg')
   glEnable(GL_TEXTURE_2D)
   glBegin(GL_QUADS)
   for i in range(0, total):
@@ -93,10 +93,10 @@ def drawGrandStandTop():
     zOffset = 0.5 * i
     yOffset = 0.25 * i
 
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5.1, 5.0 - yOffset, -5.5 + zOffset)
-    glTexCoord2f(100.0, 0.0); glVertex3f(-5.1, 5.0 - yOffset, -4.45 + zOffset)
-    glTexCoord2f(100.0, 100.0); glVertex3f(10.0, 5.0 - yOffset, -5.5 + zOffset)
-    glTexCoord2f(0.0, 100.0); glVertex3f(10.0, 5.0 - yOffset, -4.45 + zOffset)
+    glTexCoord2f(0.0, 0.0); glVertex3f(-5.1, 4.0 - yOffset, -3.0 + zOffset)
+    glTexCoord2f(0.0, 100.0); glVertex3f(-5.1, 4.0 - yOffset, -3.5 + zOffset)
+    glTexCoord2f(100.0, 100.0); glVertex3f(10.0, 4.0 - yOffset, -3.5 + zOffset)
+    glTexCoord2f(100.0, 0.0); glVertex3f(10.0, 4.0 - yOffset, -3.0 + zOffset)
     
 
   glEnd()

dff3a4133345b95153f11e89acc99ae1d0a1ab98
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-27 13:04:22 -0300

    FEAT: improve cover

--- src/drawGround.py
+++ src/drawGround.py
@@ -15,10 +15,10 @@ def drawGround():
   glTexCoord2f(0.0, 0.0); glVertex3f(3.75, 2.0, 16.0)
   glTexCoord2f(20.0, 20.0); glVertex3f(3.75, 2.0, 0.05)
 
-  glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.02, 1.4)
-  glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.02, 1.9)
-  glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.02, 1.9)
-  glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.02, 1.4)
+  glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 1.4)
+  glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 1.905) 
+  glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 1.905) 
+  glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 1.4)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)

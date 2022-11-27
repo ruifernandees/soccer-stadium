8d21f5d11ef5e33e99e97837d73d4f246ab7e11b
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-27 14:25:25 -0300

    REFACTOR: ground z

--- src/drawGround.py
+++ src/drawGround.py
@@ -16,39 +16,27 @@ def drawGround(cameraZ):
   glTexCoord2f(20.0, 20.0); glVertex3f(3.75, 2.0, 0.05)
 
   if (cameraZ >= 7.699):
-    # print ('FIRST')
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 1.4)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 1.905) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 1.905) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 1.4)
+    zFinal = 1.905
   elif (cameraZ < 7.699 and cameraZ >= 3.11):
-    # print ('SECOND')
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 1.4)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 1.925) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 1.925) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 1.4)
+    zFinal = 1.925
   else:
-    # print ('THIRD')
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 1.4)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 1.90) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 1.90) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 1.4)
+    zFinal = 1.90
+
+  glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 1.4)
+  glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, zFinal) 
+  glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, zFinal) 
+  glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 1.4)
 
   if (cameraZ >= 12.5):
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 9.9)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 9.38) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 9.38) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 9.9)
+    zFinal = 9.38
   elif (cameraZ < 12.5 and cameraZ >= 11.6):
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 9.9)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 9.36) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 9.36) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 9.9)
+    zFinal = 9.36
   else:
-    glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 9.9)
-    glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, 9.35) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, 9.34) 
-    glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 9.9)
+    zFinal = 9.35
+  glTexCoord2f(0.0, 20.0); glVertex3f(2.3, 2.035, 9.9)
+  glTexCoord2f(20.0, 0.0); glVertex3f(2.3, 2.035, zFinal) 
+  glTexCoord2f(0.0, 0.0); glVertex3f(1.11, 2.035, zFinal) 
+  glTexCoord2f(20.0, 20.0); glVertex3f(1.11, 2.035, 9.9)
 
   glEnd()
   glDisable(GL_TEXTURE_2D)

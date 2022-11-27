b9df2143b3362dd49e1382c04ef327233f0240a8
ruifernandees <dev.ruifernandes@gmail.com>
2022-11-27 12:30:28 -0300

    FEAT: draw circle

--- src/drawGroundLines.py
+++ src/drawGroundLines.py
@@ -3,10 +3,129 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from bresenham import *
 
+def CirclePlotPoints(x_centre, y_centre, x, y, z):
+    glVertex3f(x_centre + x, y_centre + y, z)
+    glVertex3f(x_centre - x, y_centre + y, z)
+    glVertex3f(x_centre + x, y_centre - y, z)
+    glVertex3f(x_centre - x, y_centre - y, z)
+    glVertex3f(x_centre + y, y_centre + x, z)
+    glVertex3f(x_centre - y, y_centre + x, z)
+    glVertex3f(x_centre + y, y_centre - x, z)
+    glVertex3f(x_centre - y, y_centre - x, z)
+
+
+def BresenhamCircleDraw(x_centre, y_centre, r, z):
+    glPushMatrix()
+    offset = 0.001
+    x = 0
+    y = r
+    CirclePlotPoints(x_centre, y_centre, x, y, z)
+
+    D = 3 - 2 * r
+
+    glColor3f(0.0, 1.0, 0.5)
+    glPointSize(3.0)
+    glBegin(GL_POINTS)
+
+    while x <= y:
+
+        if D < 0:
+            D = D + 4 * x + 2
+
+        else:
+            y -= offset
+            D = D + 4 * (x - y) + 2
+
+        CirclePlotPoints(x_centre, y_centre, x, y, z)
+        x += offset
+
+    glEnd()
+    glFlush()
+    glPopMatrix()
+
+
+# def CirclePlotPointsXZ(x_centre, z_centre, x, y, z):
+#     glVertex3f(x_centre + x, y, x_centre + z)
+#     glVertex3f(x_centre - x, y, z_centre + z)
+#     glVertex3f(x_centre + x, y, z_centre - z)
+#     glVertex3f(x_centre - x, y, z_centre - z)
+#     glVertex3f(x_centre + z, y, z_centre + x)
+#     glVertex3f(x_centre - z, y, z_centre + x)
+#     glVertex3f(x_centre + z, y, z_centre - x)
+#     glVertex3f(x_centre - z, y, z_centre - x)
+
+
+# def BresenhamCircleDrawXZ(x_centre, y_centre, z_centre, r):
+#     x = 0
+#     z = r
+#     CirclePlotPointsXZ(x_centre, z_centre, x, y_centre, z)
+
+#     D = 3 - 2 * r
+
+#     glColor3f(0.0, 1.0, 0.5)
+#     glPointSize(3.0)
+#     glBegin(GL_POINTS)
+
+#     while x <= z:
+
+#         if D < 0:
+#             D = D + 4 * x + 2
+
+#         else:
+#             z -= 1
+#             D = D + 4 * (x - z) + 2
+
+#         CirclePlotPointsXZ(x_centre, z_centre, x, y_centre, z)
+#         x += 1
+
+#     glEnd()
+#     glFlush()
+
+# def CirclePlotPointsXY(x_centre, y_centre, x, y, z):
+#     glVertex3f(x_centre + x, x_centre + y, z)
+#     glVertex3f(x_centre - x, y_centre + y, z)
+#     glVertex3f(x_centre + x, y_centre - y, z)
+#     glVertex3f(x_centre - x, y_centre - y, z)
+#     glVertex3f(x_centre + y, y_centre + x, z)
+#     glVertex3f(x_centre - y, y_centre + x, z)
+#     glVertex3f(x_centre + y, y_centre - x, z)
+#     glVertex3f(x_centre - y, y_centre - x, z)
+
+
+# def BresenhamCircleDrawXY(x_centre, y_centre, z_centre, r):
+#     x = 0
+#     y = r
+#     CirclePlotPointsXY(x_centre, y_centre, x, y, z_centre)
+
+#     D = 3 - 2 * r
+
+#     glColor3f(0.0, 1.0, 0.5)
+#     glPointSize(3.0)
+#     glBegin(GL_POINTS)
+
+#     while x <= y:
+
+#         if D < 0:
+#             D = D + 4 * x + 2
+
+#         else:
+#             y -= 1
+#             D = D + 4 * (x - y) + 2
+
+#         CirclePlotPointsXY(x_centre, y_centre, x, y, z_centre)
+#         x += 1
+
+#     glEnd()
+#     glFlush()
+
+
 def drawGroundLines():
   # lineX, lineY, lineZ = 0.0, 1, 0.0
   xOffset = 0.1
   zOffset = 0.6
+
+  x, y, z, r = 1.1 + xOffset, 2.0, 5.0 + zOffset, 2.0
+  BresenhamCircleDraw(x, y, r, z)
 
   #TOP
   line(0.0 + xOffset, 2.0, 3.0 + xOffset, 2.0, 0.0 + zOffset)
@@ -24,6 +143,9 @@ def drawGroundLines():
   line(0.1 + xOffset, 2.0, 3.0 + xOffset, 2.0, 5.0 + zOffset)
   line(0.1 + xOffset, 2.0, 3.0 + xOffset, 2.0, 5.001 + zOffset)
   line(0.1 + xOffset, 2.0, 3.0 + xOffset, 2.0, 5.002 + zOffset)
+
+
+#   BresenhamCircleDraw(x, y+0.4, z, r)
 
   # # CIRCLE CENTER
   # total = 10
